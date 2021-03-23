69b749ce64f52438c014c7106f3bf5206108e94f
John San Pietro <msanpietro@gmail.com>
2021-03-23 16:48:53 -0400

    I think I fixed the connection error I was getting

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const mongoose = require("mongoose");
 
 dotenv.config();
 
-
+app.use(express.urlencoded({ extended: true }));
 
 //connection to db
 mongoose.set("useFindAndModify", false);
+
+
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
 console.log("Connected to db!");
 app.listen(3000, () => console.log("Server Up and running"));
@@ -21,7 +23,9 @@ app.listen(3000, () => console.log("Server Up and running"));
 app.listen(3000, () => console.log("Server Up and running"));
 
 
-app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
 //get
 app.get("/", (req, res) => {
   res.render("todo.ejs");
